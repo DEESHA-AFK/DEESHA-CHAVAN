937a759f9485ba44b53fc365352749fbdd03f30d
DEESHA-AFK <chavandeeshacs242552@gmail.com>
2026-01-12 10:35:09 +0530

    M2 PRAC 11.R

--- M2 PRAC 11.R
+++ M2 PRAC 11.R
@@ -1,7 +1,5 @@
-# Load library
 library(ggplot2)
 
-# HISTOGRAM
 ggplot(data, aes(x = Sales_Amount)) +
   geom_histogram(bins = 10, fill = "skyblue", color = "black") +
   labs(
@@ -10,10 +8,10 @@ ggplot(data, aes(x = Sales_Amount)) +
     y = "Frequency"
   )
 
-# BOX PLOT
 ggplot(data, aes(y = Sales_Amount)) +
   geom_boxplot(fill = "lightgreen") +
   labs(
     title = "Box Plot of Sales Amount",
     y = "Sales Amount"
   )
+
