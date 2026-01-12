86d93ffb5dc697c4ddd317dd9ac6ebcaa7cbfdef
DEESHA-AFK <chavandeeshacs242552@gmail.com>
2026-01-12 10:45:50 +0530

    Add files via upload

--- M2 PRAC 12.R
+++ M2 PRAC 12.R
@@ -2,21 +2,18 @@ setwd("D:/S076")
 
 sales <- read.csv("sales_data.csv")
 
-print(sales)
-str(sales)
-
 sales_numeric <- sales[, sapply(sales, is.numeric)]
 
-print(sales_numeric)
+cor_matrix <- cor(sales_numeric, use = "complete.obs")
 
-cor_matrix <- cor(sales_numeric)
-print(cor_matrix)
+install.packages("corrplot")
+library(corrplot)
 
-cor_complete <- cor(sales_numeric, use = "complete.obs")
-print(cor_complete)
-
-cor_spearman <- cor(sales_numeric, method = "spearman")
-print(cor_spearman)
-
-cor_sales_profit <- cor(sales$Sales, sales$Profit, use = "complete.obs")
-print(cor_sales_profit)
+corrplot(
+  cor_matrix,
+  method = "shade",
+  type = "upper",
+  order = "hclust",
+  tl.col = "black",
+  tl.srt = 45
+)
